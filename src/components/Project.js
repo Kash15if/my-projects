15eedc5b2e0f3bee6abb60f0147e2f2c5990535c
KASHIF AHMAD <43416961+Kash15if@users.noreply.github.com>
2023-09-16 19:49:47 +0530

    added styles to projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,42 +1,54 @@
 import { Container } from "react-bootstrap";
 import { Accordion } from "react-bootstrap";
+import "./ProjectStyle.css";
 
 const imagesDir = require.context("../assets/videos/", true);
 
-
 const Projects = ({ title, videoURL, description, repo, webpage }) => {
-    return (
+  return (
+    <Container className="main-container">
+      <div className="left-section">
+        <h2 className="main-header" style={{ textAlign: "left" }}>
+          {title}
+        </h2>
+        <h2 className="sub-header" style={{ textAlign: "left" }}>
+          Sub Header : Lorem Ipsum is simply dummy text of the printing and
+          typesetting industry. Lorem Ipsum has been the industry's standard
+          dummy text ever since the 1500s,
+        </h2>
+        <Accordion>
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>{"Description"}</Accordion.Header>
+            <Accordion.Body> {description}</Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+        <div className="flex-container">
+          <a
+            href={repo}
+            className="btn"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Code
+          </a>
 
-        <Container style={{ marginTop: "55px", border: "5px solid black", padding: "20px" }}>
-
-            <h2 style={{ textAlign: "left" }}>{title}</h2>
-
-            <video width="540" height="310" controls>
-                <source src={imagesDir(`./${videoURL}`)}
-                    type="video/mp4" />
-            </video>
-
-            <Accordion >
-                <Accordion.Item eventKey="0">
-                    <Accordion.Header>{"Description"}</Accordion.Header>
-                    <Accordion.Body> {description}
-                    </Accordion.Body>
-                </Accordion.Item>
-            </Accordion>
-
-            <div className="flex-container">
-                <a href={repo} className="btn"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >Code</a>
-                <br />
-                <a href={webpage} className="btn"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >Demo</a>
-            </div>
-        </Container>
-    );
-}
+          <a
+            href={webpage}
+            className="btn"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Demo
+          </a>
+        </div>
+      </div>
+      <div className="right-section">
+        <video className="video-section" width="540" height="310" controls>
+          <source src={imagesDir(`./${videoURL}`)} type="video/mp4" />
+        </video>
+      </div>
+    </Container>
+  );
+};
 
 export default Projects;
